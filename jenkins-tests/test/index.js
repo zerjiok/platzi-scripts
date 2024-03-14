0ddbfea18cba3909ac6fe140a9cfd1b98502dfb5
Sergio Ramirez <sramirez@growthaccelerationpartners.com>
2024-03-14 07:17:03 -0600

    added new test

--- jenkins-tests/test/index.js
+++ jenkins-tests/test/index.js
@@ -11,4 +11,7 @@ describe('Sum', () => {
   it('should handle decimals', () => {
     assert.equal(sum(3.5, 7.1), 10.6);
   });
+  it('should handle sergio numbers', () => {
+    assert.equal(sum(777, 777), 8);
+  });
 });
